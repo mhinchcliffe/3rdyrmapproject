88f5ca591cd84ce2cddfafa1a4ec3c7b1110a162
mhinchcliffe <michael.hinchcliffe@hotmail.co.uk>
2015-11-28 17:37:27 +0000

    fixed most problems. still need to add exceptions and an overloaded = operator

--- mapProject/mapProject/MyMap.h
+++ mapProject/mapProject/MyMap.h
@@ -4,39 +4,81 @@
 template <typename tKey, typename tMValue> class CMyMap
 {
 public:
-	/*the the datatype being stored in the map*/
+	//the datatype being stored in the map
 	struct SValueType
 	{
 		tKey smKey;
 		tMValue smMappedValue;
 	};
+
+	//Comparison function
 	typedef bool(*fpComparrisonFunction)(tKey, tKey);
+
 private:
+	// - Iterrators - //
 	SValueType* mpFirst;//pointer to the first element in the map
+	SValueType* mpLast;//pointer to the last element in the map
+	SValueType* mpItter;//pointer to the valuetypes held in the map. used to loop through the mapped values
+
+	// - other values - //
 	int mSize;//the size of the map
 	fpComparrisonFunction fpCompare;//function to compare two keytypes. returns true if first value is higher, false otherwise
 public:
-	
 	CMyMap(fpComparrisonFunction compFunc);
 	~CMyMap();
 
 	// - Getters - //
-	//SValueType* Begin();//returns a pointer to the beggining of the array
 	bool IsEmpty();//returns true if the map is empty
-	int GetSize();//returns the number of elements in the map
+	int  GetSize();//returns the number of elements in the map
+
+	//returns a pointer to the first element of the map
+	SValueType* Begin()
+	{
+		return mpFirst;
+	}
+
+	//returns a pointer to the last element of the map
+	SValueType* End()
+	{
+		return mpLast;
+	}
+
+	//returns a pointer to the mapped value associated with the given key,
+	//returns nullptr if no such key exists
+	tMValue* GetByKeyValue(tKey key); 
+
+	//returns a pointer to the mapped value at the given index in the map,
+	//returns nullptr if index is out of range of the map
+	tMValue* GetByIndexValue(int i);
+
+	// - Overloaded Operators - //
+	tMValue* operator[](int index)
+	{
+		return GetByIndexValue(index);
+	}
+
+	
 
 	// - Interface Functions - //
 	//inserts a new element into the map, 
 	//if it doesn't already exists. returns true if inserted, false otherwise
 	bool Insert(SValueType* newElement);
 
+	//if it doesn't already exists, it inserts it into the map.
+	//if it already exists, it changes the mapped value to the new mapeed value
+	void InsertOrAssign(SValueType* newElement);
+
 	//creates and inserts a new element if the new key doesnt already exist in the map
 	//returns true if a new element is created and inserted, false otherwise
 	bool Emplace(tKey Key, tMValue mapedValue);
 
+	//creates and inserts a new element if the new key doesnt already exist in the map
+	//If the new key already exists, updates the mapped value to the newly offered mapped value
+	void EmplaceOrAssign(tKey Key, tMValue mapedValue);
+
 	//removes an element with the given key from the map
 	//returns true if the element was found and removed, false otherwise
-	bool Erase(tKey Key);
+	bool EraseByKey(tKey Key);
 
 	//removes and deletes all the elements in the current map
 	void Clear();
@@ -51,22 +93,14 @@ CMyMap<tKey, tMValue>::CMyMap(fpComparrisonFunction compFunc)
 {
 	mSize = 0;
 	fpCompare = compFunc;
-	//todo?
 }
 
 // - Destructor - //
 template<class tKey, class tMValue>
 CMyMap<tKey, tMValue>::~CMyMap()
 {
-	//todo
+	Clear();
 }
-
-//// - Getters - //
-//template<class tKey, class tMValue>
-//SValueType* CMyMap<tKey, tMValue>::Begin()
-//{
-//	return mpFirst;
-//}
 
 template<class tKey, class tMValue>
 bool CMyMap<tKey, tMValue>::IsEmpty()
@@ -84,11 +118,35 @@ int CMyMap<tKey, tMValue>::GetSize()
 	return mSize;
 }
 
+template<typename tKey, typename tMValue>
+inline tMValue * CMyMap<tKey, tMValue>::GetByKeyValue(tKey key)
+{
+	for (mpItter = mpFirst; mpItter != mpLast; mpItter++)
+	{
+		if (mpItter->smKey == key)
+		{
+
+			return &mpItter->smMappedValue;
+		}
+		else return nullptr;
+	}
+}
+
+template<typename tKey, typename tMValue>
+inline tMValue * CMyMap<tKey, tMValue>::GetByIndexValue(int i)
+{
+	if (i < mSize)
+	{
+		return &(mpFirst + i)->smMappedValue;
+	}
+	else return nullptr;
+}
+
 // - Interface Functions - //
 template<class tKey, class tMValue>
 inline bool CMyMap<tKey, tMValue>::Insert(SValueType * newElement)
 {
-	int place = 0;
+	int placeOfInsert = 0;
 	for (int i = 0; i < mSize; i++)
 	{
 		if ((mpFirst + i)->smKey == newElement->smKey)
@@ -97,30 +155,30 @@ inline bool CMyMap<tKey, tMValue>::Insert(SValueType * newElement)
 		}
 		else if (!fpCompare((mpFirst + i)->smKey, newElement->smKey))
 		{
-			place++;
+			placeOfInsert++;
 		}
 	}
 	SValueType* temp = new SValueType[mSize + 1];
 	for (int i = 0; i < mSize+1; i++)
 	{
-		if (i != place)
+		if (i != placeOfInsert)
 		{
 			*(temp+i) = *(mpFirst + i);
 		}
 		else
 		{
 			*(temp + i) = *newElement;
-			if (i != mSize)
+			if (i < mSize)
 			{
+				*(temp + i+1) = *(mpFirst + i);
 				i++;
-				*(temp + i) = *(mpFirst + i);
-			}
-			
+			}		
 		}
 	}
 	Clear();
 	mSize++;	
 	mpFirst = temp;
+	mpLast = temp + (mSize - 1);
 	return true;//todo
 }
 
@@ -143,30 +201,108 @@ inline bool CMyMap<tKey, tMValue>::Emplace(tKey Key, tMValue mapedValue)
 }
 
 template<class tKey, class tMValue>
-inline bool CMyMap<tKey, tMValue>::Erase(tKey Key)
+inline bool CMyMap<tKey, tMValue>::EraseByKey(tKey Key)
 {
-	return false;//todo
+	bool isInMap = false;
+	int Position=0;
+	for (mpItter = mpFirst; mpItter != mpLast&&!isInMap; mpItter++)
+	{
+		Position++;
+		if (mpItter->smKey == Key)
+		{
+			isInMap = true;
+			Position--;
+		}
+	}
+	if (isInMap)
+	{
+		SValueType* temp = new SValueType[mSize - 1];
+		mpItter = mpFirst;
+		for (int i = 0; i < mSize-1; i++)
+		{
+			if (i!=Position)
+			{
+				*(temp + i) = *(mpItter);
+				mpItter++;
+			}
+			else
+			{
+				mpItter++;
+				*(temp + i) = *(mpItter);
+				mpItter++;
+			}
+		}
+		Clear();
+		mSize--;
+		mpFirst = temp;
+		mpLast = (temp + mSize - 1);
+	}
+	return isInMap;
 }
 
 template<class tKey, class tMValue>
 inline void CMyMap<tKey, tMValue>::Clear()
 {
-	for (int i = 0; i < mSize; i++)
-	{
-		if (mpFirst)
-		{
-			delete (mpFirst+i);
-		}
-	}
+	delete mpFirst;
+	
 	mpFirst = nullptr;
-	//todo
+	mpLast = nullptr;
 }
 
 template<typename tKey, typename tMValue>
 inline void CMyMap<tKey, tMValue>::PrintMap()
 {
+	for (mpItter = mpFirst; mpItter <= mpLast; mpItter++)
+	{
+		std::cout << mpItter->smMappedValue <<std::endl;
+	}
+}
+
+template<typename tKey, typename tMValue>
+inline void CMyMap<tKey, tMValue>::InsertOrAssign(SValueType * newElement)
+{
+	int placeOfInsert = 0;
 	for (int i = 0; i < mSize; i++)
 	{
-		std::cout << (mpFirst+i)->smKey<<std::endl;
+		if ((mpFirst + i)->smKey == newElement->smKey)
+		{
+			(mpFirst + i)->smMappedValue = newElement->smMappedValue;
+			return;
+		}
+		else if (!fpCompare((mpFirst + i)->smKey, newElement->smKey))
+		{
+			placeOfInsert++;
+		}
 	}
+	SValueType* temp = new SValueType[mSize + 1];
+	for (int i = 0; i < mSize + 1; i++)
+	{
+		if (i != placeOfInsert)
+		{
+			*(temp + i) = *(mpFirst + i);
+		}
+		else
+		{
+			*(temp + i) = *newElement;
+			if (i < mSize)
+			{
+				*(temp + i + 1) = *(mpFirst + i);
+				i++;
+			}
+		}
+	}
+	Clear();
+	mSize++;
+	mpFirst = temp;
+	mpLast = temp + (mSize - 1);
+}
+
+template<typename tKey, typename tMValue>
+inline void CMyMap<tKey, tMValue>::EmplaceOrAssign(tKey Key, tMValue mapedValue)
+{
+	SValueType* temp = new SValueType;
+	temp->smKey = Key;
+	temp->smMappedValue = mapedValue;
+	InsertOrAssign(temp);
+	delete temp;
 }
--- mapProject/mapProject/Source.cpp
+++ mapProject/mapProject/Source.cpp
@@ -8,10 +8,37 @@ bool afunction(int i, int j)
 int main()
 {
 
-	CMyMap<int, int> aMap(&afunction);
-	aMap.Emplace(1, 2);
-	aMap.Emplace(2, 3);
-	/*aMap.Emplace(3, 4);*/
+	CMyMap<int, char> aMap(&afunction);
+	aMap.Emplace(4, 'a');
+	aMap.Emplace(2, 'c');
+	aMap.Emplace(3, 'b');
+	std::cout << "test emplace" << std::endl;
 	aMap.PrintMap();
+
+	aMap.EraseByKey(2);
+	std::cout << "test erase" << std::endl;
+	aMap.PrintMap();
+	std::cout << "test getByKeyValue" << std::endl;
+	std::cout << *aMap.GetByKeyValue(3)<<std::endl;
+	std::cout << "test Using getByKeyValue to change mapped value" << std::endl;
+	*aMap.GetByKeyValue(3) = 'l';
+	std::cout << *aMap.GetByKeyValue(3) << std::endl;
+
+	aMap.EmplaceOrAssign(3, 'J');
+	aMap.EmplaceOrAssign(9, 't');
+	std::cout << "test EmplaceOrAssign" << std::endl;
+	aMap.PrintMap();
+
+	std::cout << "test overloaded[] operator" << std::endl;
+
+	std::cout << *aMap.GetByIndexValue(0)<<std::endl;
+	std::cout << *aMap[0] << std::endl;
+
+	//std::cout << "test overloaded = operator" << std::endl;
+
+	//CMyMap<int, char> newMap(&afunction);
+	//newMap = aMap;
+	//aMap.Clear();
+	//newMap.PrintMap();
 	std::system("pause");
 }
